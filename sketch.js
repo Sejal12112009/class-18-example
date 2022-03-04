651348694c1c15820ba67793ae8b095d25e491f1
Sejal12112009 <97644497+Sejal12112009@users.noreply.github.com>
2022-03-05 10:13:44 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var S1
+var S2
+function setup() {
+  createCanvas(400,400);
+S1 = new Student("Sejal",12,8)
+S1.display()
+
+S2 = new Student("Avika",6,1)
+S2.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
